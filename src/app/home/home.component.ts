c8c3811a83deda8ebff0a26fec61ec808dd727d4
poomikam <rohikaforever45@gmail.com>
2024-01-21 18:01:56 +0530

    Your commit message

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
+declare var $: any; // Declare $ to use jQuery
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
   images = [
     "./assets/images/cake1.jpg",
     "./assets/images/cup.jpg",
     "./assets/images/cake2.jpg",
   ];
+
+  ngAfterViewInit(): void {
+    // Initialize Bootstrap carousel after the view is initialized
+    $('#cakeCarousel').carousel();
+  }
 }
